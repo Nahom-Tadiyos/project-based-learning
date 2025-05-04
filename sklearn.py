e37b45edafc1f7116a9fa2eb3bfcfeb067bdfa5c
Nahom <8nahom8@gmail.com>
2025-05-04 18:14:50 -0500

    sklearn start

---
+++ sklearn.py
@@ -0,0 +1,15 @@
+import sklearn
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv("imgs\tested.csv")
+data.info()
+print(data)
