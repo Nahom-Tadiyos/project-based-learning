f266244a4f0079539e48c96f2f75b1b7d75febd7
Nahom <8nahom8@gmail.com>
2025-05-04 20:52:21 -0500

    added code to procces data

--- sklearn.py
+++ sklearn.py
@@ -2,6 +2,11 @@
 import pandas as pd
 import numpy as np
 
+from sklearn.model_selection import *
+from sklearn.preprocessing import *
+from sklearn.neighbors import *
+from sklearn.metrics import *
+
 from sklearn import *
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -16,7 +21,7 @@ def preprocces_data(df):
 
     df["Embarked"].fillna("S", inplace = True)
     df.drop(columns=["Embarked"], inplace = True)
-    
+
     fill_missing_ages(df)
 
     #Convert Gender
@@ -39,3 +44,16 @@ def fill_missing_ages(df):
     df["Age"] = df.apply(lambda row: age_fill_map[row["Pclass"]] if pd.isnull(row['Age']) else row["Age"],
                          axis = 1)
     
+data = preprocces_data(data)
+
+#Create Features / Target Variables (Make Flashcards)
+X = data.drop(columns=["Survived"])
+y = data["Survived"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42) 
+
+#ML Preprocessing
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
