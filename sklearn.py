a9ca818ae4bffea29aec88b3ca1cbfe35b53a25c
Nahom <8nahom8@gmail.com>
2025-05-04 20:36:55 -0500

    added func to preprocces the data

--- sklearn.py
+++ sklearn.py
@@ -1,15 +1,29 @@
-import sklearn
+
 import pandas as pd
 import numpy as np
 
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
-
+from sklearn import *
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv("imgs\tested.csv")
+data = pd.read_csv("imgs/tested.csv")
 data.info()
 print(data)
+
+#data cleaning
+def preprocces_data(df):
+    df.drop(columns=["PassengerId","Survived","Pclass","Name","Sex","Age","SibSp","Parch","Ticket","Fare","Cabin","Embarked"])
+
+    df["Embarked"].fillna("S", inplace = True)
+    df.drop(columns=["Embarked"], inplace = True)
+
+    #Convert Gender
+    df["Sex"] = df["Sex"].map({'male':1, "female":0})
+
+    #Feature enginerring
+    df["FamilySize"] = df["SibSp"] + df["Parch"]
+    df["IsAlone"] = np.where(df['FamilySize'] == 0, 1, 0)
+    df["FareBin"] = pd.qcut()(df["Fare"], 4, labels=False)
+    df["AgeBin"] = pd.cut(df["Age"], bins=[0,12,20,40,60, np.inf], labels = False)
+
+    return df
