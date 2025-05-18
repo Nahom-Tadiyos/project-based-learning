dcfded37cad3a86cd8878658fd1345a65587ba57
Nahom <8nahom8@gmail.com>
2025-05-17 20:28:41 -0500

    added graphing and evaluating each model

--- mlmastery.py
+++ mlmastery.py
@@ -18,12 +18,50 @@ url = "datasets/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
-# shape the data set
-print(dataset.shape)
-#(150,5) 150 instances and 5 attributes
+# # shape the data set
+# print(dataset.shape)
+# #(150,5) 150 instances and 5 attributes
 
-# descriptions/ Statistical Summary
-print(dataset.describe())
+# # descriptions/ Statistical Summary
+# print(dataset.describe())
 
-# class distribution
-print(dataset.groupby('class').size())
+# # class distribution
+# print(dataset.groupby('class').size())
+
+#DATA VISUALIZATION
+#box and whisker plots
+# dataset.plot(kind="box", subplots=True, layout=(2,2))
+# plt.show()
+
+#histograms
+# dataset.hist()
+# plt.show()
+
+#scatter plot matrix
+# scatter_matrix(dataset)
+# plt.show()
+
+#Evaluate some algorithms
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=0.20, random_state=1)
+
+#Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class="ovr")))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+#evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring="accuracy")
+    results.append(cv_results)
+    names.append(name)
+    print('%s:%f(%f)' % (name, cv_results.mean(), cv_results.std()))
+    
